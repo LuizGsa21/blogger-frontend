072decb3f4716b515b57c58e94cb18cef2f915ed
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-12-02 12:34:40 -0600

    refactor(test): use ES6 syntax

--- src/app/components/messaging/messaging.service.spec.js
+++ src/app/components/messaging/messaging.service.spec.js
@@ -1,29 +1,29 @@
-describe('service messaging', function () {
-  var mock;
+describe('service messaging', () => {
+  var mock, messaging, event = Symbol('event');
   beforeEach(angular.mock.module('bloggerFrontend'));
 
-  beforeEach(function () {
+  beforeEach(inject((_messaging_) => {
+    messaging = _messaging_;
     mock = {
-      callback: function () {}
+      callback: () => {}
     };
     spyOn(mock, 'callback');
-  });
-
-  it('should be defined', inject(function (messaging) {
-    expect(messaging).toBeDefined();
   }));
 
-  it("should expose it's API ", inject(function (messaging) {
+  it('should be defined', () => {
+    expect(messaging).toBeDefined();
+  });
+
+  it("should expose it's API ", () => {
     expect(typeof messaging.subscribe).toBe('function');
     expect(typeof messaging.unsubscribe).toBe('function');
     expect(typeof messaging.publish).toBe('function');
-  }));
+  });
 
-  it('should be able to add subscribers and publish topics', inject(function (messaging) {
-    messaging.subscribe('testing', mock.callback);
-    messaging.publish('testing', [1, 2, 3]);
+  it('should be able to add subscribers and publish topics', () => {
+    messaging.subscribe(event, mock.callback);
+    messaging.publish(event, [1, 2, 3]);
     expect(mock.callback).toHaveBeenCalledWith(1, 2, 3);
-  }));
-
+  });
 
 });
