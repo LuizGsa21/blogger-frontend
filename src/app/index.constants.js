24065a429da45d99c8e86a0d859ccbdd4e625d77
Luiz Arantes Sa <LArantesSa@icloud.com>
2016-01-16 13:42:13 -0600

    refactor(constants): update BASE_API_URL

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,3 +1,3 @@
-export const BASE_API_URL = 'http://localhost:7001/api/v1';
+export const BASE_API_URL = 'http://blogger.com/api/v1';
 export const USER_LOGIN_URL = `${BASE_API_URL}/auth/login`;
 export const USER_LOGOUT_URL = `${BASE_API_URL}/auth/logout`;
